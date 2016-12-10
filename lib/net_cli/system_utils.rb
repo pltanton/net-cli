53b5c685d94e108f47d24a0a427b098d928c0aa4
Plotnikov Anton <plotnikovanton@gmail.com>
2016-12-10 18:38:48 +0300

    wifi scan parsing

--- lib/net_cli/system_utils.rb
+++ lib/net_cli/system_utils.rb
@@ -1,18 +1,42 @@
+# frozen_string_literal: true
+# Utils to communicate with system
 module SystemUtils
   # Returns first wlp iface from iwconfig
   def self.wlan_iface
-    `iwconfig | grep wlp`.split.first
+    `ls /sys/class/net | grep wl`.split.first
   end
 
+  # Returns list of triple about each founded network in format [essid,
+  # level, encryption type].
   def self.wlan_list(iface)
-    result = []
-    `iwlist #{iface} scan | grep 'Frequency\\|ESSID'`
-      .split("\n").map(&:strip).each_slice 2 do |a, e|
-      result << {
-        essid: e[/^ESSID:"(.*)"$/, 1],
-        freq: a[/^Frequency:(\d+\.\d+) GHz .*/, 1]&.to_f
-      }
-    end
-    result.sort_by { |e| -e[:freq] }
+    `iwlist #{iface} scan`
+      .split(/\n(?=\s+Cell)/)[1..-1]
+      .map { |e| e.split("\n").map(&:strip) }
+      .map { |e| parse_wlan_entity e }
+      .sort_by { |e| -e[1] }
   end
+
+  def self.parse_wlan_entity(element)
+    essid = nil
+    level = nil
+    type = nil
+    element.each do |row|
+      essid = row[/(?<=ESSID:").*(?="$)/] unless essid
+      level = row[/(?<=Signal level=)\-\d+(?=\sdBm$)/]&.to_i unless level
+      type = parse_enc_type row unless type
+    end
+    [essid, level, type]
+  end
+  private_class_method :parse_wlan_entity
+
+  def self.parse_enc_type(row)
+    if row.include? 'WPA'
+      :wpa
+    elsif row.include? 'WEP'
+      :wep
+    elsif row.include? 'Encryption key:off'
+      :none
+    end
+  end
+  private_class_method :parse_enc_type
 end
--- lib/net_cli/wifi.rb
+++ lib/net_cli/wifi.rb
@@ -2,15 +2,30 @@ require 'net_cli/system_utils'
 require 'net_cli/template_generator'
 
 class Wifi < Thor
-  option :essid
-  option :hidden, type: :boolean
-  option :iface
-  option :type
-  desc 'new OPTIONS', 'generates net entry in /etc/netctl/'
+  desc 'add', 'interactive generates net entry in /etc/netctl/'
   def new
     iface = options[:iface] || SystemUtils::wlan_iface
-    puts iface
+    ip = options[:ip] || 'dhcp'
     wlan_list = SystemUtils::wlan_list iface
-    p wlan_list
+    answer = ask_about_lan wlan_list
+    wlan = wlan_list[answer]
+    key = ask_about_password wlan
+    puts TemplateGenerator::wireless type: wlan[0], essid: wlan[2], ip: ip,
+                                     key: key
+  end
+
+  private
+
+  def ask_about_lan(wlan_list)
+    puts 'Please select a network'
+    wlan_list.each_with_index do |entity, i|
+      puts ('%2i) %-15s %3i dBm [%s]' % ([i] + entity))
+    end
+    ask('Please select:').to_i
+  end
+
+  def ask_about_password(wlan)
+    retun if wlan[2] == :none
+    ask 'Put a password:'
   end
 end
